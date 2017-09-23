76b5131856628bc21f62d699d14cca859ca3afd5
Mike Frampton <mikeframpo@gmail.com>
2017-09-23 18:56:08 +1200

    expanding the statblock component added json data from https://www.reddit.com/r/dndnext/comments/43a09o/srd_monsters_in_json_format/

--- krumm-ui/src/app/creep.ts
+++ krumm-ui/src/app/creep.ts
@@ -1,6 +1,9 @@
 
 export class Creep {
   name: string = 'Animated Armor';
-  type: string = 'Medium construct';
-  alignment: string = 'unaligned';
+  type: string = 'Medium construct, unaligned';
+  ac: string = '18 (natural armor)';
+  hp: string = '33 (6d8 + 6)';
+  speed: string = '25ft';
+  stats: number[] = [14, 11, 13, 1, 3, 1];
 }
--- krumm-ui/src/app/statblock.component.ts
+++ krumm-ui/src/app/statblock.component.ts
@@ -1,6 +1,10 @@
 import { Component } from "@angular/core";
 
 import { Creep } from './creep';
+import { getStatModLookup } from "./stats";
+
+const noEditStyle: string = "creep-field-noedit";
+const statNames: string[] = ['STR', 'DEX', 'CON', 'INT', 'WIS', 'CHA'];
 
 @Component({
   selector: 'stat-block',
@@ -9,5 +13,26 @@ import { Creep } from './creep';
 })
 export class Statblock {
 
+  editable: boolean = false;
   creep: Creep = new Creep();
+
+  getEditStyle(): string {
+    if (this.editable) {
+      return "";
+    }
+    return noEditStyle;
+  }
+
+  toggleEditable(): void {
+    this.editable = !this.editable;
+  }
+
+  getStatNames(): string[] {
+    return statNames;
+  }
+
+  getStats(): string[] {
+    return this.creep.stats.map(
+      item => item.toString() + '(' + getStatModLookup(item) + ')');
+  }
 }
