cff1f4fd55be543707115fe8a3fd396ceceb5518
Mike Frampton <mikeframpo@gmail.com>
2017-10-02 18:18:59 +1300

    adding more fields to creep model

--- krummserver/creeps/json_creep_parser.py
+++ krummserver/creeps/json_creep_parser.py
@@ -1,18 +1,74 @@
 
 import json
+from django.core.exceptions import ValidationError
 
-from creeps.models import Size, Creep
+from creeps.models import Creep, Size, Type, Subtype, Alignment
 
-def parse_json_creeps(apps, schema_editor, json_path):
+def get_string(creep_data, name, required=True):
+    val = creep_data[name]
+    if val is None or len(val) == 0:
+        if required:
+            raise ValidationError(
+                        'Required field %s not present' % name)
+        else:
+            return None
+    return val
+
+def get_int(creep_data, name, required=True):
+    val = creep_data[name]
+    if val is None:
+        if required:
+            raise ValidationError(
+                        'Required field %s not present' % name)
+        else:
+            return None
+    return int(val)
+
+def parse_json_creeps(json_path):
 
     parsed = json.load(open(json_path))
-    creep_data = parsed[0]
-    
-    size = Size.objects.get_or_create(size=creep_data['size'].lower())
+    for creep_data in parsed:
 
-    name = creep_data['name'].lower()
-    print('Adding creep: ' + name)
+        if 'license' in creep_data.keys():
+            continue
 
-    Creep.objects.get_or_create(name=name,
-                                size=size[0])
+        creep_size = get_string(creep_data, 'size')
+        size, added = Size.objects.get_or_create(size=creep_size.lower())
+
+        creep_type = get_string(creep_data, 'type')
+        type, added = Type.objects.get_or_create(type=creep_type.lower())
+
+        creep_subtype = get_string(creep_data, 'subtype', required=False)
+        if creep_subtype is not None:
+            subtype, added = Subtype.objects.get_or_create(
+                                                subtype=creep_subtype.lower())
+        else:
+            subtype = None
+
+        creep_align = get_string(creep_data, 'alignment')
+        alignment, added = Alignment.objects.get_or_create(
+                                            alignment=creep_align.lower())
+
+        armor_class = get_int(creep_data, 'armor_class')
+        hit_points = get_int(creep_data, 'hit_points')
+        speed = get_string(creep_data, 'speed')
+
+        strength = get_int(creep_data, 'strength')
+        dexterity = get_int(creep_data, 'dexterity')
+        constitution = get_int(creep_data, 'constitution')
+        intelligence = get_int(creep_data, 'intelligence')
+        wisdom = get_int(creep_data, 'wisdom')
+        charisma = get_int(creep_data, 'charisma')
+
+        name = creep_data['name'].lower()
+        print('Processing creep: ' + name)
+
+        creep, created = Creep.objects.get_or_create(name=name, woc=True,
+                size=size, type=type, subtype=subtype, alignment=alignment,
+                armor_class=armor_class, hit_points=hit_points, speed=speed,
+                strength=strength, dexterity=dexterity, 
+                constitution=constitution, intelligence=intelligence,
+                wisdom=wisdom, charisma=charisma)
+
+        creep.save()
 
--- krummserver/creeps/models.py
+++ krummserver/creeps/models.py
@@ -1,18 +1,40 @@
 from django.db import models
 from django.core.exceptions import ValidationError
 
-CHARFIELD_DEFAULT = 50
-
-def validate_nonempty(value):
-    if len(value) == 0:
-        raise ValidationError('field must be non-empty')
+CHARFIELD_DEFAULT = 100
 
 class Size(models.Model):
-    size=models.CharField(max_length=CHARFIELD_DEFAULT,
-                            validators=[validate_nonempty])
+    size=models.CharField(max_length=CHARFIELD_DEFAULT)
+
+class Type(models.Model):
+    type=models.CharField(max_length=CHARFIELD_DEFAULT)
+
+class Subtype(models.Model):
+    subtype=models.CharField(max_length=CHARFIELD_DEFAULT)
+
+class Alignment(models.Model):
+    alignment=models.CharField(max_length=CHARFIELD_DEFAULT)
 
 class Creep(models.Model):
-    name=models.CharField(max_length=CHARFIELD_DEFAULT,
-                            validators=[validate_nonempty])
+    name=models.CharField(max_length=CHARFIELD_DEFAULT)
+    woc=models.BooleanField()
+
     size=models.ForeignKey(Size, on_delete=models.CASCADE)
+    type=models.ForeignKey(Type, on_delete=models.CASCADE)
+    subtype=models.ForeignKey(Subtype, on_delete=models.CASCADE, null=True)
+    alignment=models.ForeignKey(Alignment, on_delete=models.CASCADE)
+
+    armor_class = models.IntegerField()
+    hit_points = models.IntegerField()
+    #TODO: hit dice
+    speed = models.CharField(max_length=CHARFIELD_DEFAULT)
+
+    strength = models.IntegerField()
+    dexterity = models.IntegerField()
+    constitution = models.IntegerField()
+    intelligence = models.IntegerField()
+    wisdom = models.IntegerField()
+    charisma = models.IntegerField()
+
+    senses = models.CharField(max_length=CHARFIELD_DEFAULT)
 
