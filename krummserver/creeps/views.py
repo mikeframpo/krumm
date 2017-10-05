81412827e0e71ed4aaf783183636b5a89914106a
Mike Frampton <mikeframpo@gmail.com>
2017-10-05 21:44:55 +1300

    getting more fields in creep request

--- krummserver/creeps/views.py
+++ krummserver/creeps/views.py
@@ -18,6 +18,50 @@ def get_creep_json(creep, fields):
 
     if has_field('name'):
         creep_obj['name'] = string.capwords(creep.name)
+    if has_field('size'):
+        creep_obj['size'] = string.capwords(creep.size.size)
+    if has_field('type'):
+        creep_obj['type'] = creep.type.type
+    if creep.subtype and has_field('subtype'):
+        creep_obj['subtype'] = creep.subtype.subtype
+    if has_field('alignment'):
+        creep_obj['alignment'] = creep.alignment.alignment
+
+    if has_field('armor_class'):
+        creep_obj['armor_class'] = creep.armor_class
+    if has_field('hit_points'):
+        creep_obj['hit_points'] = creep.hit_points
+    if has_field('hitdice_num'):
+        creep_obj['hitdice_num'] = creep.hitdice_num
+    if has_field('hitdice_type'):
+        creep_obj['hitdice_type'] = creep.hitdice_type
+    if has_field('speed'):
+        creep_obj['speed'] = creep.speed
+
+    if has_field('strength'):
+        creep_obj['strength'] = creep.strength
+    if has_field('dexterity'):
+        creep_obj['dexterity'] = creep.dexterity
+    if has_field('constitution'):
+        creep_obj['constitution'] = creep.constitution
+    if has_field('intelligence'):
+        creep_obj['intelligence'] = creep.intelligence
+    if has_field('wisdom'):
+        creep_obj['wisdom'] = creep.wisdom
+    if has_field('charisma'):
+        creep_obj['charisma'] = creep.charisma
+
+    if has_field('senses'):
+        creep_obj['senses'] = creep.senses
+
+    if has_field('skills'):
+        for creep_skill in creep.skills.order_by('skill').iterator():
+            creep_obj[creep_skill.skill.skill] = creep_skill.modifier
+
+    if has_field('saving_throws'):
+        for st in creep.saving_throws.order_by('ability').iterator():
+            creep_obj[st.ability.ability + '_save'] \
+                    = st.modifier
 
     return json.dumps(creep_obj)
 
